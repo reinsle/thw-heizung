e6b427ed3f6429ffdec513237e38f3422411f238
Robert Einsle <robert@einsle.de>
2013-12-24 13:48:38 +0100

    Creating crud for events table
    Signed-off-by: Robert Einsle <robert@einsle.de>

---
+++ protected/views/events/_view.php
@@ -0,0 +1,47 @@
+<?php
+/* @var $this EventsController */
+/* @var $data Events */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('uid')); ?>:</b>
+	<?php echo CHtml::encode($data->uid); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('dtstamp')); ?>:</b>
+	<?php echo CHtml::encode($data->dtstamp); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('dtstart')); ?>:</b>
+	<?php echo CHtml::encode($data->dtstart); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('dtend')); ?>:</b>
+	<?php echo CHtml::encode($data->dtend); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('category')); ?>:</b>
+	<?php echo CHtml::encode($data->category); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('summary')); ?>:</b>
+	<?php echo CHtml::encode($data->summary); ?>
+	<br />
+
+	<?php /*
+	<b><?php echo CHtml::encode($data->getAttributeLabel('description')); ?>:</b>
+	<?php echo CHtml::encode($data->description); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('location')); ?>:</b>
+	<?php echo CHtml::encode($data->location); ?>
+	<br />
+
+	*/ ?>
+
+</div>
