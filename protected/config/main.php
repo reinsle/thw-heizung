54696a71fbd27ba9365760145e5e175725e538c8
Robert Einsle <robert@einsle.de>
2013-12-24 13:23:14 +0100

    Adding project configuration

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'THW Heizung',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -20,14 +20,12 @@ return array(
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'ni.xd.ol',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('127.0.0.1','::1'),
+			'ipFilters'=>array('127.0.0.1','::1','10.4.5.124'),
 		),
-		*/
 	),
 
 	// application components
@@ -47,19 +45,19 @@ return array(
 			),
 		),
 		*/
-		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
-		// uncomment the following to use a MySQL database
-		/*
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
-			'charset' => 'utf8',
-		),
-		*/
+        /*
+        'db'=>array(
+            'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+        ),
+        */
+        // uncomment the following to use a MySQL database
+        'db'=>array(
+            'connectionString' => 'pgsql:host=10.4.3.30;dbname=heizung',
+            'emulatePrepare' => true,
+            'username' => 'heizung',
+            'password' => 'ni.xd.ol',
+            'charset' => 'utf8',
+        ),
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
