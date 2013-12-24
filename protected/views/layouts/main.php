45b4c0b42a33f49201f2d676c223769d3631b660
Robert Einsle <robert@einsle.de>
2013-12-24 16:18:46 +0100

    changing company

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -49,7 +49,7 @@
 	<div class="clear"></div>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+		Copyright &copy; <?php echo date('Y'); ?> by einsle.de.<br/>
 		All Rights Reserved.<br/>
 		<?php echo Yii::powered(); ?>
 	</div><!-- footer -->
