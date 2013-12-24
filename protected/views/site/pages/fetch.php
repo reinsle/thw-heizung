7c7e6d5787a106bd3c01c866b4cc1eed3a38a626
Robert Einsle <robert@einsle.de>
2013-12-24 17:27:13 +0100

    Adding parser for ical stuff

--- protected/views/site/pages/fetch.php
+++ protected/views/site/pages/fetch.php
@@ -10,16 +10,40 @@ $this->breadcrumbs=array(
 
 
 <?php
-  $url='http://ov-kempten.ov-cms.thw.de/unser-thw-ortsverband/terminkalender/kalender/ics/?type=150&tx_cal_controller%5Bcalendar%5D=20';
 
-  $temp = tempnam('/tmp', 'thw-kempten');
+    function icsToArray($paramUrl) {
+        $icsFile = file_get_contents($paramUrl);
+        $icsData = explode("BEGIN:", $icsFile);
+        foreach($icsData as $key => $value) {
+            $icsDatesMeta[$key] = explode("\n", $value);
+        }
+        foreach($icsDatesMeta as $key => $value) {
+            foreach($value as $subKey => $subValue) {
+                if ($subValue != "") {
+                    if ($key != 0 && $subKey == 0) {
+                        $icsDates[$key]["BEGIN"] = $subValue;
+                    } else {
+                        $subValueArr = explode(":", $subValue, 2);
+                        $icsDates[$key][$subValueArr[0]] = $subValueArr[1];
+                    }
+                }
+            }
+        }
+        return $icsDates;
+    } 
 
-  echo $temp;
+    $url='http://ov-kempten.ov-cms.thw.de/unser-thw-ortsverband/terminkalender/kalender/ics/?type=150&tx_cal_controller%5Bcalendar%5D=20';
 
-  file_put_contents($temp, file_get_contents($url));
+    $temp = tempnam('/tmp', 'thw-kempten');
 
+    file_put_contents($temp, file_get_contents($url));
 
-  unlink($temp);
+    $data = icsTOArray($temp);
+
+    echo '<br />';
+    echo json_encode($data);
+
+    unlink($temp);
 
 ?>
 <p>This is a "static" page. You may change the content of this page
