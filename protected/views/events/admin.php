6f540715604219b992d73931a65185a9e109490a
Robert Einsle <robert@einsle.de>
2013-12-24 16:16:25 +0100

    Changing columns to display

--- protected/views/events/admin.php
+++ protected/views/events/admin.php
@@ -45,17 +45,16 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 	'dataProvider'=>$model->search(),
 	'filter'=>$model,
 	'columns'=>array(
-		'id',
 		'uid',
 		'dtstamp',
 		'dtstart',
 		'dtend',
 		'category',
-		/*
 		'summary',
+		/*
 		'description',
-		'location',
 		*/
+		'location',
 		array(
 			'class'=>'CButtonColumn',
 		),
