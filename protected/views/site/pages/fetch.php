7e81d447d871317570466e74175c1dea0b7bf205
Robert Einsle <robert@einsle.de>
2013-12-25 21:47:40 +0100

    moving out code to function

--- protected/views/site/pages/fetch.php
+++ protected/views/site/pages/fetch.php
@@ -31,6 +31,43 @@ $this->breadcrumbs=array(
         return $icsDates;
     }
 
+    /*
+     * Read out the data out of the input array and parse them to event-data
+     *
+     * $value Value-Array holding the data
+     * $event Event-Object to store
+     */
+    function parseData($value, $event) {
+        if (array_key_exists('DTSTAMP', $value)) {
+            $ts = strtotime($value['DTSTAMP']);
+            $event->dtstamp = date(DateTime::RFC1123, $ts);
+        }
+        if (array_key_exists('DTSTART', $value)) {
+            $ts = strtotime($value['DTSTART']);
+            $event->dtstart = date(DateTime::RFC1123, $ts);
+        }
+        if (array_key_exists('DTEND', $value)) {
+            $ts = strtotime($value['DTEND']);
+            $event->dtend = date(DateTime::RFC1123, $ts);
+        }
+        if (array_key_exists('SUMMARY', $value)) {
+            if ($event->summary != trim($value['SUMMARY'])) {
+                $event->summary = trim($value['SUMMARY']);
+            }
+        }
+        if (array_key_exists('DESCRIPTION', $value)) {
+            if ($event->description != trim($value['DESCRIPTION'])) {
+                $event->description = trim($value['DESCRIPTION']);
+            }
+        }
+        if (array_key_exists('LOCATION', $value)) {
+            if ($event->location != trim($value['LOCATION'])) {
+                $event->location = trim($value['LOCATION']);
+            }
+        }
+        $event->save();
+    }
+
     date_default_timezone_set('Europe/Berlin');
     $url='http://ov-kempten.ov-cms.thw.de/unser-thw-ortsverband/terminkalender/kalender/ics/?type=150&tx_cal_controller%5Bcalendar%5D=20';
     $temp = tempnam('/tmp', 'thw-kempten');
@@ -49,55 +86,13 @@ $this->breadcrumbs=array(
                 if (count($models) > 0) {
                     // Update Date to Events model
                     $event = $models[0];
-                    if (array_key_exists('DTSTAMP', $value)) {
-                        $ts = strtotime($value['DTSTAMP']);
-                        $event->dtstamp = date(DateTime::RFC1123, $ts);
-                    }
-                    if (array_key_exists('DTSTART', $value)) {
-                        $ts = strtotime($value['DTSTART']);
-                        $event->dtstart = date(DateTime::RFC1123, $ts);
-                    }
-                    if (array_key_exists('DTEND', $value)) {
-                        $ts = strtotime($value['DTEND']);
-                        $event->dtend = date(DateTime::RFC1123, $ts);
-                    }
-                    if (array_key_exists('SUMMARY', $value)) {
-                        $event->summary = trim($value['SUMMARY']);
-                    }
-                    if (array_key_exists('DESCRIPTION', $value)) {
-                        $event->description = trim($value['DESCRIPTION']);
-                    }
-                    if (array_key_exists('LOCATION', $value)) {
-                        $event->location = trim($value['LOCATION']);
-                    }
-                    $event->save();
+                    parseData($value, $event);
                     $update++;
                 } else {
                     // Create new Events model from data
                     $event = new Events();
                     $event->uid = trim($value['UID']);
-                    if (array_key_exists('DTSTAMP', $value)) {
-                        $ts = strtotime($value['DTSTAMP']);
-                        $event->dtstamp = date(DateTime::RFC1123, $ts);
-                    }
-                    if (array_key_exists('DTSTART', $value)) {
-                        $ts = strtotime($value['DTSTART']);
-                        $event->dtstart = date(DateTime::RFC1123, $ts);
-                    }
-                    if (array_key_exists('DTEND', $value)) {
-                        $ts = strtotime($value['DTEND']);
-                        $event->dtend = date(DateTime::RFC1123, $ts);
-                    }
-                    if (array_key_exists('SUMMARY', $value)) {
-                        $event->summary = trim($value['SUMMARY']);
-                    }
-                    if (array_key_exists('DESCRIPTION', $value)) {
-                        $event->description = trim($value['DESCRIPTION']);
-                    }
-                    if (array_key_exists('LOCATION', $value)) {
-                        $event->location = trim($value['LOCATION']);
-                    }
-                    $event->save();
+                    parseData($value, $event);
                     $insert++;
                 }
             }
