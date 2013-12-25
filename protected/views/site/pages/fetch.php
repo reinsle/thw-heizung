336f02fc8d785d89d3d2e7f942fb76531572adc5
Robert Einsle <robert@einsle.de>
2013-12-25 20:39:53 +0100

    adding data to database

--- protected/views/site/pages/fetch.php
+++ protected/views/site/pages/fetch.php
@@ -30,12 +30,15 @@ $this->breadcrumbs=array(
             }
         }
         return $icsDates;
-    } 
+    }
 
+    date_default_timezone_set('Europe/Berlin');
     $url='http://ov-kempten.ov-cms.thw.de/unser-thw-ortsverband/terminkalender/kalender/ics/?type=150&tx_cal_controller%5Bcalendar%5D=20';
     $temp = tempnam('/tmp', 'thw-kempten');
     file_put_contents($temp, file_get_contents($url));
     $data = icsTOArray($temp);
+    $insert = 0;
+    $update = 0;
     foreach ($data as &$value) {
         if ($value['BEGIN'] == 'VEVENT') {
             $criteria = new CDbCriteria;
@@ -44,7 +47,18 @@ $this->breadcrumbs=array(
             if (count($models) > 0) {
                 // Update Date to Events model
                 $event = $models[0];
-
+                if (array_key_exists('DTSTAMP', $value)) {
+                    $ts = strtotime($value['DTSTAMP']);
+                    $event->dtstamp = date(DateTime::RFC1123, $ts);
+                }
+                if (array_key_exists('DTSTART', $value)) {
+                    $ts = strtotime($value['DTSTART']);
+                    $event->dtstart = date(DateTime::RFC1123, $ts);
+                }
+                if (array_key_exists('DTEND', $value)) {
+                    $ts = strtotime($value['DTEND']);
+                    $event->dtend = date(DateTime::RFC1123, $ts);
+                }
                 if (array_key_exists('SUMMARY', $value)) {
                     $event->summary = trim($value['SUMMARY']);
                 }
@@ -55,11 +69,23 @@ $this->breadcrumbs=array(
                     $event->location = trim($value['LOCATION']);
                 }
                 $event->save();
+                $update++;
             } else {
                 // Create new Events model from data
                 $event = new Events();
                 $event->uid = trim($value['UID']);
-
+                if (array_key_exists('DTSTAMP', $value)) {
+                    $ts = strtotime($value['DTSTAMP']);
+                    $event->dtstamp = date(DateTime::RFC1123, $ts);
+                }
+                if (array_key_exists('DTSTART', $value)) {
+                    $ts = strtotime($value['DTSTART']);
+                    $event->dtstart = date(DateTime::RFC1123, $ts);
+                }
+                if (array_key_exists('DTEND', $value)) {
+                    $ts = strtotime($value['DTEND']);
+                    $event->dtend = date(DateTime::RFC1123, $ts);
+                }
                 if (array_key_exists('SUMMARY', $value)) {
                     $event->summary = trim($value['SUMMARY']);
                 }
@@ -70,14 +96,13 @@ $this->breadcrumbs=array(
                     $event->location = trim($value['LOCATION']);
                 }
                 $event->save();
+                $insert++;
             }
-            echo 'laber <br />';
         }
     }
     unlink($temp);
     unset($url);
     unset($temp);
     unset($data);
+    echo '<p>es wurden ' . $insert . ' neue Datensaetze angelegt, und ' . $update . ' Datensaetze aktualisiert.</p>';
 ?>
-<p>This is a "static" page. You may change the content of this page
-by updating the file <code><?php echo __FILE__; ?></code>.</p>
