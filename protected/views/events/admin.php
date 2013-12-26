41803061e54d5afd585f0039a8972417b65cf7cc
Robert Einsle <robert@einsle.de>
2013-12-26 09:23:03 +0100

    rearrange columns in admin view

--- protected/views/events/admin.php
+++ protected/views/events/admin.php
@@ -46,15 +46,11 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 	'filter'=>$model,
 	'columns'=>array(
 		'uid',
-		'dtstamp',
 		'dtstart',
 		'dtend',
-		'category',
 		'summary',
-		/*
 		'description',
 		'location',
-		*/
 		array(
 			'class'=>'CButtonColumn',
 		),
