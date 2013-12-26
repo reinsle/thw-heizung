2623deb182583b3b26a80de25707f456b2aac4c0
Robert Einsle <robert@einsle.de>
2013-12-26 19:24:18 +0100

    Add sample date into migration script

--- protected/migrations/m131226_162226_create_user_table.php
+++ protected/migrations/m131226_162226_create_user_table.php
@@ -4,6 +4,7 @@ class m131226_162226_create_user_table extends CDbMigration
 {
     public function up()
     {
+        date_default_timezone_set('Europe/Berlin');
         $this->createTable('tbl_user', array(
             'id' => 'pk',
             'email' => 'varchar(128) NOT NULL',
@@ -13,6 +14,12 @@ class m131226_162226_create_user_table extends CDbMigration
             'last_login_time' => 'int',
         ));
         $this->createIndex('tbl_user_email_unique', 'tbl_user', 'email', true);
+        $this->insert('tbl_user', array(
+            "email" => "admin@einsle.de",
+            "password" => sha1("ni.xd.ol"),
+            "create_time" => strtotime('now'),
+            "update_time" => strtotime('now'),
+        ));
     }
 
     public function down()
