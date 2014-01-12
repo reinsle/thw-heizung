a4ef499eeb094668138b3ecb4dc5f52bcef8fe0b
Robert Einsle <robert@einsle.de>
2014-01-12 17:01:41 +0100

    fix path info

--- protected/config/main.php
+++ protected/config/main.php
@@ -45,6 +45,7 @@ return array(
             'allowAutoLogin' => true,
         ),
         // uncomment the following to enable URLs in path-format
+        /*
         'urlManager'=>array(
             'urlFormat'=>'path',
             'rules'=>array(
@@ -53,6 +54,7 @@ return array(
                 '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
             ),
         ),
+        */
         'db' => array(
             'connectionString' => 'sqlite:' . dirname(__FILE__) . '/../data/heizung.db',
         ),
