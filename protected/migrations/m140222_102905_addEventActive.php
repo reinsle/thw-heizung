11cbb5caff71c91907a8f74741ecb3f1a54b89ef
Robert Einsle <robert@einsle.de>
2014-02-22 11:35:56 +0100

    add active column to event table

---
+++ protected/migrations/m140222_102905_addEventActive.php
@@ -0,0 +1,14 @@
+<?php
+
+class m140222_102905_addEventActive extends CDbMigration
+{
+	public function safeUp()
+	{
+        $this->addColumn('tbl_event', 'active', 'boolean NOT NULL DEFAULT 1');
+	}
+
+	public function safeDown()
+	{
+        $this->dropColumn('tbl_event', 'active');
+	}
+}
