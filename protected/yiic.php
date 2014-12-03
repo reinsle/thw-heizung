13f16f5544826a999a93635fa053cd65cf35e9cc
Robert Einsle <robert@einsle.de>
2014-12-03 10:21:44 +0100

    move yii-link to new framework path

--- protected/yiic.php
+++ protected/yiic.php
@@ -1,7 +1,7 @@
 <?php
 
 // change the following paths if necessary
-$yiic = dirname(__FILE__) . '/../../yii/framework/yiic.php';
+$yiic = '/usr/share/frameworks/yii/yii-1.1.15.022a51/framework/yiic.php';
 $config = dirname(__FILE__) . '/config/console.php';
 
 require_once($yiic);
